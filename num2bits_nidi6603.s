d98a64ce6910b1fb57750a3d90e319e23ef687e7
Nick DiSette <144621130+nickdisettee@users.noreply.github.com>
2025-04-15 09:51:20 -0600

    Update num2bits_nidi6603.s
    Resubmission complete

--- num2bits_nidi6603.s
+++ num2bits_nidi6603.s
@@ -1,61 +1,59 @@
+# Verified 
+
+.global num2bits
+
+# num stored in r4
+num2bits:
+	addi    sp, sp, -28
+    
+    stw     ra, 24(sp)      # prologue
+    stw     r21, 20(sp)
+    stw     r20, 16(sp)
+    stw     r19, 12(sp)
+    stw     r18, 8(sp)
+    stw     r17, 4(sp)
+    stw     r16, 0(sp)
+
+    movi    r19, 0
+    movi    r20, 0
+
+    addi    r16, r0, 9999
+    bgt     r4, r16, num2bits_err
+    blt     r4, r0, num2bits_err
+
+digit_loop:
+    addi    r17, r0, 10
+    div     r21, r4, r17
+    mul     r18, r21, r17
+    sub     r18, r4, r18
+    ldbu    r16, Bits7seg(r18)
+    muli    r17, r20, 8
+    sll     r16, r16, r17
+    add     r19, r19, r16
+    addi    r20, r20, 1
+    beq     r21, r0, num2bits_end
+    mov     r4, r21
+    br      digit_loop
+
+num2bits_err:
+    addi    r19, r0, 0x4040 
+    slli    r19, r19, 16
+    addi    r19, r19, 0x4040
+
+num2bits_end:
+    add     r2, r19, r0     # put return value in r2
+    
+    ldw     ra, 24(sp)      # epilogue
+    ldw     r21, 20(sp)
+    ldw     r20, 16(sp)
+    ldw     r19, 12(sp)
+    ldw     r18, 8(sp)
+    ldw     r17, 4(sp)
+    ldw     r16, 0(sp)
+    
+    addi    sp, sp, 28
+    ret
+
 .data
 Bits7seg:
-    .byte 0x3F  # 0
-    .byte 0x06  # 1
-    .byte 0x5B  # 2
-    .byte 0x4F  # 3
-    .byte 0x66  # 4
-    .byte 0x6D  # 5
-    .byte 0x7D  # 6
-    .byte 0x07  # 7
-    .byte 0x7F  # 8
-    .byte 0x6F  # 9
-
-.text
-.global _start
-
-num2bits_nonrecursive:
-    # Input: r3 = number (0-9999)
-    # Output: r2 = 7-segment bit pattern
-
-    addi r2, r0, 0          # r2 = 0 (initialize output)
-    addi r4, r0, 0          # r4 = 0 (counter)
-    addi r5, r0, 10         # r5 = 10 (for division)
-
-extract_digits:
-    div r6, r3, r5          # r6 = r3 / 10 (quotient)
-    mul r7, r6, r5          # r7 = r6 * 10
-    sub r8, r3, r7          # r8 = r3 - r7 (remainder, i.e., the digit)
-    ldbu r9, Bits7seg(r8)   # r9 = 7-segment bit pattern for the digit
-    slli r2, r2, 8          # Shift r2 left by 8 bits
-    or r2, r2, r9           # Combine the new digit with the existing bits
-    add r3, r0, r6          # r3 = quotient for next iteration
-    addi r4, r4, 1          # Increment counter
-    bne r3, r0, extract_digits # If quotient != 0, continue extracting digits
-
-    ret                     # Return with r2 containing the bit pattern
-
-num2bits_recursive:
-    # Input: r3 = number (0-9999)
-    # Output: r2 = 7-segment bit pattern
-
-    addi r5, r0, 10         # r5 = 10 (for division)
-    blt r3, r5, single_digit # If r3 < 10, handle as a single digit
-
-    # Recursive case
-    div r6, r3, r5          # r6 = r3 / 10 (quotient)
-    mul r7, r6, r5          # r7 = r6 * 10
-    sub r7, r3, r7          # r7 = r3 - r7 (remainder, i.e., the digit)
-    subi sp, sp, 4          # Allocate space on the stack
-    stw r7, 0(sp)           # Save the remainder (digit) on the stack
-    call num2bits_recursive  # Recursively call the function with the quotient
-    ldw r7, 0(sp)           # Restore the remainder (digit) from the stack
-    addi sp, sp, 4          # Deallocate space from the stack
-    ldbu r8, Bits7seg(r7)   # r8 = 7-segment bit pattern for the digit
-    slli r2, r2, 8          # Shift r2 left by 8 bits
-    or r2, r2, r8           # Combine the new digit with the existing bits
-    ret
-
-single_digit:
-    ldbu r2, Bits7seg(r3)   # r2 = 7-segment bit pattern for the single digit
-    ret
+    .byte   0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F
