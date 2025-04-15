19a3a6ad1f33a4366f756e6589ae955c65d2f20e
Nick DiSette <144621130+nickdisettee@users.noreply.github.com>
2025-04-15 09:46:53 -0600

    Update factorial_nidi6603.s
    Resubmission complete

--- factorial_nidi6603.s
+++ factorial_nidi6603.s
@@ -1,13 +1,19 @@
-#n in r4
-FACTORIAL:
-    addi r2, r0, 1
-    addi r3, r0, 1
+# Verified
 
-LOOP:
-    bgt r4, r3, MULTIPLY
+# N in r4
+.global factorial
+factorial:
+    addi  sp, sp, -4
+    stw   ra, 0(sp)
+    movi  r2, 1
+
+factorial_loop:
+    beq   r4, r0, factorial_end
+    mul   r2, r4, r2
+    addi  r4, r4, -1
+    br    factorial_loop
+
+factorial_end:
+    ldw   ra, 0(sp)
+    addi  sp, sp, 4
     ret
-
-MULTIPLY:
-    mul r2, r2, r4
-    addi r4, r4, -1
-    br LOOP
